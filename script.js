be89b27a8ef873e2b5c79035cbd8645773c07f80
Brenda <ndirangubrenda44@gmail.com>
2023-05-09 15:26:01 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,63 @@
+//slider
+const AmountRange = document.getElementById('AmountRange')
+const AmountNumber = document.getElementById('AmountNumber')
+
+const UppercaseElement = document.getElementById('Uppercase')
+const NumbersElement = document.getElementById('Numbers')
+const SymbolsElement = document.getElementById('Symbols')
+
+const form = document.getElementById('passwordGeneratorForm')
+
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+AmountNumber.addEventListener('input', syncCharacterAmount)
+AmountRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = AmountNumber.value
+  const Uppercase = UppercaseElement.checked
+  const Numbers = NumbersElement.checked
+  const Symbols = SymbolsElement.checked
+  const password = generatePassword(characterAmount, Uppercase, Numbers, Symbols)
+  passwordDisplay.innerText = password
+})
+
+function generatePassword(characterAmount, Uppercase, Numbers, Symbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (Uppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (Symbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (Numbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  AmountNumber.value = value
+  AmountRange.value = value
+}
